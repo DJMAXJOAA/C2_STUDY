ca485a6a41183ccb8f05fc3b7ed35533d25f9da5
djmaxjoaa <cpsn6237@gmail.com>
2023-05-25 12:18:41 +0900

    초기 커밋.

---
+++ C2_STUDY/C2_STUDY.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <cmath> // sqrt 사용
+using namespace std;
+
+/* 입출력문자 */
+//int main()
+//{
+//	using namespace std;	// 미리 명시해서 std:: 작성 생략
+//	cout << "Hello C++!!" << endl;	// 문자 출력, endl 개행문자
+//	int a;
+//	cin >> a; // 문자 입력
+//
+//	a = a << 1; // 비트 연산자
+//	cout << a << endl;
+//}
+
+/* 입출력 타이밍 연습 */
+//int main()
+//{
+//	using namespace std;
+//
+//	int carrots;
+//
+//	carrots = 25;
+//	cout << "나는 당근을 ";
+//	cout << carrots;
+//	cout << "개 가지고 있다." << endl;
+//	carrots -= 1;
+//	cout << "아삭 아삭, 이제 당근은 " << carrots << "개 가지고 있다." << endl;
+//
+//	return 0;
+//}
+
+/* puts */
+//int main()
+//{
+//	char ch = 'M';
+//	int i = ch;
+//	cout << ch << "ASCII코드는 " << i << "d입니다" << endl;
+//
+//	cout << "이 문자 코드에 1을 더해보겠습니다." << endl;
+//	ch += 1;
+//	i = ch;
+//	cout << ch << "의 ASCII코드는" << i << "입니다" << endl;
+//
+//	cout << "cout.put(ch)를 사용하여 char형 변수 ch를 화면에 출력 : ";
+//	cout.put(ch);
+//
+//	cout.put('!');
+//
+//	cout << endl << "종료" << endl;
+//	return 0;
+//}
+
+int main()
+{
+
+}
